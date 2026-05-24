325a92b417e24a06fef26b4158f09cb385694703
eduardosb11 <barbosa.eduardo11@outlook.com>
2026-05-24 16:32:28 -0300

    adicionar script

---
+++ script.js
@@ -0,0 +1,58 @@
+// Funções
+const obterElemento = (query) => document.querySelector(query);
+const gerarNumeroSecreto = () => Math.floor(Math.random() * 100 + 1);
+const atualizarTentativas = () => obterElemento("#tentativas").innerText = tentativasRestantes;
+const obterPalpite = () => parseInt(obterElemento("#palpite").value);
+const atualizarMensagem = (msg) => obterElemento("#mensagem").innerText = msg;
+
+// Variaveis
+const tentativasMaximas = 5;
+let tentativasRestantes = tentativasMaximas;
+let numeroSecreto = gerarNumeroSecreto();
+let jogoTerminou = false;
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (jogoTerminou) {
+        return;
+    }
+    chutar();
+});
+
+iniciarJogo();
+
+function chutar() {
+    let palpite = obterPalpite();
+    let mensagem = "";
+
+    if (palpite > 100 || palpite < 1) {
+        mensagem = "Palpite inválido! Insira um número entre 1 e 100."
+        return;
+    }
+
+    if (palpite == numeroSecreto) {
+        mensagem = "Você acertou!";
+        jogoTerminou = true;
+    } else if (palpite > numeroSecreto) {
+        mensagem = "O número secreto é menor";
+    } else {
+        mensagem = "O número secreto é maior";
+    }
+
+    tentativasRestantes--;
+    atualizarTentativas();
+
+    if (tentativasRestantes <= 0 && palpite != numeroSecreto) {
+        mensagem = `Você perdeu! O número secreto era ${numeroSecreto}`;
+        jogoTerminou = true;
+    }
+
+    atualizarMensagem(mensagem);
+};
+
+function iniciarJogo() {
+    numeroSecreto = gerarNumeroSecreto();
+    tentativasRestantes = tentativasMaximas;
+    atualizarTentativas();
+}
